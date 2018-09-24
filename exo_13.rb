a165c891f5c47e8eca5af44df153b671ca01be17
margauxCortier <cortier.margaux@gmail.com>
2018-09-24 14:37:44 +0200

    exo 13 done

--- exo_13.rb
+++ exo_13.rb
@@ -2,11 +2,12 @@ puts "hello c'est quoi ton année de naissance ?"
 birth_year = gets.to_i
 
 i = birth_year
+now = Time.new.year
 
 loop do 
 	i += 1
 	puts i
-	if i == 2018
+	if i == now
 	break
 end
 end
